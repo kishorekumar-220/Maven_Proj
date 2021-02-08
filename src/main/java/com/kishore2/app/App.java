111f62d929f9458e29c5387547d0d42724bd758d
Kishorekumar <kk088933@gmail.com>
2021-02-08 07:20:47 +0530

    First commit

---
+++ src/main/java/com/kishore2/app/App.java
@@ -0,0 +1,31 @@
+package com.kishore2.app;
+import java.util.Scanner;
+
+/**
+ * Floyd's Triangle
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       int n, num = 1, c, d;
+    Scanner in = new Scanner(System.in);
+ 
+    System.out.println("Enter the number of rows of Floyd's triangle to display");
+    n = in.nextInt();
+ 
+    System.out.println("Floyd's triangle:");
+ 
+    for (c = 1; c <= n; c++)
+    {
+      for (d = 1; d <= c; d++)
+      {
+        System.out.print(num+" ");
+        num++;
+      }
+ 
+      System.out.println(); // Moving to next row
+    }
+    }
+}
